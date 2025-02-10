a3ddf7a8f6e66ea9b5e5472190ee3b9c5fb56bef
dev-jiwon <devjiwon@gmail.com>
2025-02-10 09:46:58 +0900

    -

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "KlipIosSdkForSpm",
+    name: "KlipLib-simulator",
     platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
